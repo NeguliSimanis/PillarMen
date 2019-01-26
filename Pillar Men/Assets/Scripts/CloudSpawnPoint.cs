324434a3c68b36310056a41f89e04e8ed44adb03
NeguliSimanis <simanism@inbox.lv>
2019-01-26 16:38:14 +0200

    0.0.8 - moving clouds

---
+++ Pillar Men/Assets/Scripts/CloudSpawnPoint.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] cloudsToSpawn;
+    int lastSpawnedCloudID = -1;
+    int nextSpawnedCloudID;
+    int cloudArrayLength;
+
+    //float cloudLifeTime = 7f;
+    float cloudRespawnTime = 0f;
+    float cloudRespawnDelay = 9.2f;
+
+    void Start()
+    {
+        SpawnCloud();
+        Debug.Log("clouds " + cloudsToSpawn.Length);
+    }
+
+    void SpawnCloud()
+    {
+        cloudRespawnTime = Time.time + cloudRespawnDelay;
+        ChooseCloudToSpawn();
+        GameObject newCloud = Instantiate(cloudsToSpawn[nextSpawnedCloudID], gameObject.transform);
+        newCloud.transform.position = transform.position;
+        lastSpawnedCloudID = nextSpawnedCloudID;
+    }
+
+    private void Update()
+    {
+        if (Time.time > cloudRespawnTime)
+        {
+            SpawnCloud();
+        }
+    }
+
+    void ChooseCloudToSpawn()
+    {
+        if (lastSpawnedCloudID == -1)
+        {
+            nextSpawnedCloudID = Random.Range(0, cloudsToSpawn.Length-1);
+        }
+        else
+        {
+            int tempCloudID = Random.Range(0, cloudsToSpawn.Length - 1);
+            if (tempCloudID == lastSpawnedCloudID && tempCloudID != cloudsToSpawn.Length-1)
+            {
+                nextSpawnedCloudID++;
+            }
+        }
+    }
+}
---
+++ Pillar Men/Assets/Scripts/Clouds.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+    //SpriteRenderer spriteRenderer;
+    public SpriteRenderer cloudSprite;
+    private float fadeOutTime = 2f;
+    private float fadeInTime = 2f;
+    private float delayAfterFadeIn = 5f; // how long after fading in does the cloud start to fade out
+
+    private bool isFadedIn = false;
+    private bool isFadingOut = false;
+    private float fadeOutStartTime;
+
+    private float cloudMoveSpeed = 0.01f;
+    private void Start()
+    {
+        cloudSprite = GetComponent<SpriteRenderer>();
+        FadeIn();
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector2(transform.position.x + cloudMoveSpeed, transform.position.y);// + dirNormalized * PlayerData.current.moveSpeed * Time.deltaTime;
+            
+        if (isFadedIn)
+        {
+            if (Time.time > fadeOutStartTime && !isFadingOut)
+            {
+                isFadingOut = true;
+                FadeOut();
+            }
+        }
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeCloud(cloudSprite, 0, 1, true, fadeInTime));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeCloud(cloudSprite, cloudSprite.material.color.a, 0, false, fadeOutTime));
+    }
+
+    public IEnumerator FadeCloud(SpriteRenderer cg, float start, float end, bool fadeIn, float lerpTime = 1)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.material.color = new Color(cg.material.color.r, cg.material.color.g, cg.material.color.b, currentValue);
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForFixedUpdate();
+        }
+        if (fadeIn)
+        {
+            isFadedIn = true;
+            fadeOutStartTime = Time.time + delayAfterFadeIn;
+        }
+        else
+        {
+            DestroyCloud();
+        }
+    }
+    void DestroyCloud()
+    {
+        Destroy(gameObject);
+    }
+}
