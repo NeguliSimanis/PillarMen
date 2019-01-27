5bf8470deaf554ab9d235cd184d4be120b921383
NeguliSimanis <simanism@inbox.lv>
2019-01-27 07:59:20 +0200

    Adam attacks damage player

---
+++ Pillar Men/Assets/Scripts/BossHand.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHand : MonoBehaviour
+{
+    int bossDamage = 44;
+    float damageDealDelay = 1f; // how much time must pass before player can be damaged again
+    float nextDamageTime = -1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DealDamageToPlayer(collision.gameObject.GetComponent<PlayerController>());      
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DealDamageToPlayer(collision.gameObject.GetComponent<PlayerController>());
+        }
+    }
+
+    private void DealDamageToPlayer(PlayerController playerController)
+    {
+        if (nextDamageTime == -1 || Time.time > nextDamageTime)
+        {
+            nextDamageTime = Time.time + damageDealDelay;
+            playerController.TakeDamage(bossDamage);
+        }
+    }
+}
